ad05d60af1554784df60c5c159442c9424347785
tanveer <shaiktanveerulhuq@gmail.com>
2018-05-15 22:32:36 +0530

    1. Created first python file in pycharm to start doing leetcode problems

--- 1. two sum.py
+++ 1. two sum.py
@@ -1,2 +1,9 @@
-print("Hello")
-print("Hello Ha")
+class Solution:
+    def twoSum(self, nums, target):
+        for i in nums:
+            if target-i in nums[nums.index(i)+1:]:
+                break
+        return [nums.index(i),nums.index(i)   +    nums[nums.index(i)+1:].index(target-i) + 1]
+
+#supporting code
+print(Solution.twoSum(10,[3,2,1,3],6))
